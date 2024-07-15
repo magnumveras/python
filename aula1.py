aa0df8424d86f2a6f0b8f1993efb57a8b403d835
Magno Veras <magno.veras88@gmail.com>
2024-07-15 16:25:07 -0300

    Aula1

---
+++ aula1.py
@@ -0,0 +1,12 @@
+# Qualquer teste 
+# Qualquer teste 
+# Qualquer teste 
+# Qualquer teste 
+# Qualquer teste 
+# Qualquer teste 
+# Qualquer teste 
+# Qualquer teste 
+# Qualquer teste 
+# Qualquer teste 
+print('Opa, editei!')
+print('Atenção')
