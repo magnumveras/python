c4d93aaa645d6ce449eec45c615913a0b6a26579
Magno Veras <magno.veras88@gmail.com>
2024-07-24 16:12:58 -0300

    Aula3-fim

--- aula3.py
+++ aula3.py
@@ -1,10 +1,24 @@
 """
+Docstring
 Python = Linguagem de programação
 Tipo de tipagem = Dinâmica / Forte
 str -> string -> texto
 """
+
+print(1234)
+
 # Aspas simples
+print('Magno Veras')
 
 # Aspas duplas 
+print("Magno Veras")
 
 # Escape
+print("Magno \"Veras\"")
+
+# r
+print(r"Magno \"Veras\"")
+
+print("Magno 'Veras'")
+print(1, "Magno 'Veras'")
+print(2, 'Magno "Veras"')
