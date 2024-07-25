076b0e374453d1acaed9d4d5f7c45addc223e23e
Magno Veras <magno.veras88@gmail.com>
2024-07-25 15:19:34 -0300

    aula8

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Magno'
+sobrenome = 'Veras'
+idade = 36
+ano_nascimento = 2024-36
+maior_de_idade = idade >= 18
+altura_metros = 1.84
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
