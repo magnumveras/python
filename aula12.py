058cf497c12f34880a25a1197af16e8f866d6208
Magno Veras <magno.veras88@gmail.com>
2024-07-25 23:12:46 -0300

    aula12

---
+++ aula12.py
@@ -0,0 +1,11 @@
+nome = 'Magno Veras'
+altura = 1.84
+peso = 105
+imc = peso / (altura ** 2)
+
+# Magno Veras tem 1.84 de altura,
+# pesa 95 quilos e seu imc é 
+# 12873717231
+
+print(nome, 'tem', altura, 'de altura',
+      'pesa', peso, 'quilos e seu imc é', imc)
