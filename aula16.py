5e0b7c3bb898704a2e4e86a8282aae6cbf6da5e1
Magno Veras <magno.veras88@gmail.com>
2024-07-30 17:03:13 -0300

    condicionais

---
+++ aula16.py
@@ -0,0 +1,19 @@
+# if / else        / else
+# se / se não se / se não 
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+
+    print(12312312312)
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Você não digitou nem "entrar" e nem "sair".')
+    
+
+print('FORA DOS BLOCOS')
+
+
+
+
