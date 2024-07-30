369de8856359b65470a936d27d6cde54f24490b4
Magno Veras <magno.veras88@gmail.com>
2024-07-30 16:33:15 -0300

    inputs

---
+++ aula15.py
@@ -0,0 +1,6 @@
+# nome = input('Qual o seu nome? ')
+numero_1 = input('Digite um número? ')
+numero_2 = input('Digite outro número? ')
+
+print(f'A soma dos números é: {int(numero_1) + int(numero_2)}')
+
