edfc0f74641a94ec6506bcb1e3ce3491e743d7f5
Magno Veras <magno.veras88@gmail.com>
2024-07-30 17:16:21 -0300

    fluxo condicional

---
+++ aula17.py
@@ -0,0 +1,23 @@
+# if / else        / else
+# se / se não se / se não 
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    ...
+elif condicao3:
+    pass
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('nenhuma condição foi satisfeita!')
+ 
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
