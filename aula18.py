a4cd64c422ec723e1264ebece2a766af6c1fbf8b
Magno Veras <magno.veras88@gmail.com>
2024-07-31 15:44:44 -0300

    debuf

---
+++ aula18.py
@@ -0,0 +1,21 @@
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    ...
+elif condicao3:
+    pass
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('nenhuma condição foi satisfeita!')
+ 
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
